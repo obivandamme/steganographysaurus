1f6101803447fc91c2ba2d4c99aa4955cbcef724
Pascal Martin <pascal.martin@phocassoftware.com>
2024-02-21 13:04:56 +1300

    add option to choose between hide and reveal in the console

--- Steganographysaurus.Console/Program.cs
+++ Steganographysaurus.Console/Program.cs
@@ -2,21 +2,47 @@
 using Steganographysaurus.Infrastructure;
 using Steganographysaurus.Core;
 
-var message = $"Hello World!\x1A";
-var pwd = "123456789";
 var source = "Data/steganographysaurus.png";
 var target = "Data/steganographysaurus-hidden.png";
-
 var repository = new BitmapStegoImageRepository();
 var service = new SteganographyService(repository);
 
-Console.WriteLine("Hiding message...");
-service.HideMessage(message, pwd, source, target);
-Console.WriteLine("Done!");
+Console.WriteLine("Welcome to Steganographysaurus");
+Console.WriteLine("Hiding and revealing messages in images");
+Console.WriteLine("=======================================");
+Console.WriteLine();
+Console.WriteLine("Do you want to hide or reveal a message?");
+Console.WriteLine("1. Hide");
+Console.WriteLine("2. Reveal");
 
-Console.WriteLine("Revealing message...");
-var revealedMessage = service.RevealMessage(pwd, target);
-Console.WriteLine("Done!");
+var option = Console.ReadLine();
+string pwd;
+string message;
 
-Console.WriteLine(revealedMessage);
+switch (option)
+{
+	case "1":
+		Console.WriteLine("Enter a message to hide in the image:");
+		var input = Console.ReadLine();
+		message = $"{input}\x1A";
+
+		Console.WriteLine("Enter a password to protect the message (optional):");
+		pwd = Console.ReadLine() ?? "123456";
+
+		Console.WriteLine("Hiding message...");
+		service.HideMessage(message, pwd, source, target);
+		Console.WriteLine("Done!");
+		break;
+	case "2":
+		Console.WriteLine("Enter the password to reveal the message (optional):");
+		pwd = Console.ReadLine() ?? "123456";
+		Console.WriteLine("Revealing message...");
+		message = service.RevealMessage(pwd, target);
+		Console.WriteLine(message);
+		break;
+	default:
+		Console.WriteLine("Invalid option");
+		return;
+}
+
 Console.ReadLine();
